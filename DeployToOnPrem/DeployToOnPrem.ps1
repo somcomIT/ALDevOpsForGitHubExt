3897968560edb062e13d3516e40d0bdd5bcb131e
Martin (Docker) <martin.sompek@somcom.at>
2023-01-30 13:27:10 +0100

    test param tenantid

--- DeployToOnPrem/DeployToOnPrem.ps1
+++ DeployToOnPrem/DeployToOnPrem.ps1
@@ -5,7 +5,8 @@ param(
     $token,
     $branch,
     $syncMode,
-    $uninstallMode
+    $uninstallMode,
+    $aadTenantId
 )
 
 function importModuleWithTestPath
@@ -129,7 +130,17 @@ if ($allArtifacts) {
             $UnpublishedVersion = $App.Version
             $runner += 1
         }
-        Publish-NAVApp -ServerInstance $BCInstance -Path $appFile -SkipVerification -scope Tenant
+        if ($aadTenantId) {
+            Write-Host "Tenant=$($aadTenantId)"
+        } else {
+            Write-Host "no Tenant spec"
+        }
+
+        #if ($aadTenantId) {
+        #    Publish-NAVApp -ServerInstance $BCInstance -Path $appFile -SkipVerification -scope Tenant -PublisherAzureActiveDirectoryTenantId $aadTenantId
+        #} else {
+            Publish-NAVApp -ServerInstance $BCInstance -Path $appFile -SkipVerification -scope Tenant
+        #}
         $App = Get-NAVAppInfo -ServerInstance $BCInstance -TenantSpecificProperties -name $appName -Tenant 'default' | Where-Object { $_.IsPublished }
         Write-Host "$("{0:d2}" -f $runner): publishing $($App.Name), $($App.Version) to $BCInstance"
         Write-Host '... publish'
